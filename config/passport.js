8528d12b50dad8dca0b18f0f93d8a1ae979e3c32
Ross Topol <rtopol2@gmail.com>
2017-10-18 22:06:11 -0500

    fix passport config typo

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,8 @@
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require("passport-local");
 
-const User = require('../app/models/user');
+const User = require("../app/models/user");
 
 module.exports = function(passport) {
-
   // =========================================
   // SESSION SETUP ===========================
   // =========================================
@@ -21,7 +20,6 @@ module.exports = function(passport) {
       done(err, user);
     });
   });
-};
 
   // =========================================
   // LOCAL SIGNUP ============================
@@ -29,50 +27,55 @@ module.exports = function(passport) {
   // we are using named strategies since we have one for login and one for signup
   // by default, if there was none, it would just be called 'local'
 
-  passport.use('local-signup', new LocalStrategy({
-    // by default, local strategy uses usernameand password
-    // we will override with email
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true // allows us to pass back entire request to the callback
-  },
-  function(req, email, password, done) {
-
-    // async
-    // User.findOne won't fire unless data is sent back
-    process.nextTick(function() {
-
-      // find a user whose email is the same as form's email
-      // we are checking to see if the user trying to login already exists
-      User.findOne({ 'local.email': email }, function(err, user) {
-        // if there are any errors, return the error
-        if (err) {
-          return done(err);
-        }
-
-        // check to see if there's already a user with that email
-        if (user) {
-          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-        } else {
-
-          // if there is no user with that email
-          // create the user
-          const newUser = new User();
-
-          //set the user's local credentails
-          newUser.local.email = email;
-          newUser.local.password = newUser.generateHash(password);
-
-          // save the user
-          newUser.save(function(err) {
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses usernameand password
+        // we will override with email
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback: true // allows us to pass back entire request to the callback
+      },
+      function(req, email, password, done) {
+        // async
+        // User.findOne won't fire unless data is sent back
+        process.nextTick(function() {
+          // find a user whose email is the same as form's email
+          // we are checking to see if the user trying to login already exists
+          User.findOne({ "local.email": email }, function(err, user) {
+            // if there are any errors, return the error
             if (err) {
-              throw err;
+              return done(err);
             }
-            return done(null, newUser);
-          });
-        }
-      });
-    })
-  }
 
-))
+            // check to see if there's already a user with that email
+            if (user) {
+              return done(
+                null,
+                false,
+                req.flash("signupMessage", "That email is already taken.")
+              );
+            } else {
+              // if there is no user with that email
+              // create the user
+              const newUser = new User();
+
+              //set the user's local credentails
+              newUser.local.email = email;
+              newUser.local.password = newUser.generateHash(password);
+
+              // save the user
+              newUser.save(function(err) {
+                if (err) {
+                  throw err;
+                }
+                return done(null, newUser);
+              });
+            }
+          });
+        });
+      }
+    )
+  );
+};
